2ed445cdf3eb9a271454e5aa925cdbdde2052cba
Cyril Deschamps <77438408+CyrilDlaPompote@users.noreply.github.com>
2022-04-04 21:43:25 +0200

    Graph et Dijkstra

---
+++ src/graph.rs
@@ -0,0 +1,96 @@
+
+pub struct NodeLink { // CHEMIN
+  toNode: String,
+  weigth: i32
+}
+
+pub struct Node { // NOEUD
+  name_id: String,
+  links: Vec<NodeLink>,
+  calculWeight: i32,
+}
+
+pub struct Graph {
+  nodes: Vec<Node>,
+}
+
+impl Graph {
+
+  pub fn new() -> Graph {
+    return Graph { nodes: Vec::new() }
+  }
+
+  pub fn show(&self) {
+    print!("########### AFFICHAGE GRAPH #############\n");
+    for i in 0..self.nodes.len() {
+      print!("\nNoeud {:?}: (poids calculé: {:?})\n", self.nodes[i].name_id, self.nodes[i].calculWeight);
+      for j in 0..self.nodes[i].links.len() {
+        print!("    - Vers noeud {:?} -> Poids: {:?}\n", self.nodes[i].links[j].toNode, self.nodes[i].links[j].weigth);
+      }
+    }
+  }
+
+  pub fn addPoint(&mut self, name_id: String) {
+    self.nodes.push(Node { name_id, links: Vec::new(), calculWeight: i32::MAX })
+  }
+
+  pub fn linkPoints(&mut self, name_id: String, to_name_id: String, weight: i32) -> bool {
+    let mut i = 0;
+    let mut j = 0;
+    let mut i_found = false;
+    let mut j_found = false;
+
+    while i < self.nodes.len() && !i_found { //ON VERIF SI LE PREMIER POINT EXISTE
+      if self.nodes[i].name_id == name_id {
+        while j < self.nodes.len() && !j_found { //SI OUI, ON VERIF SI LE DEUXIEME POINT EXISTE
+          if self.nodes[j].name_id == name_id {
+            self.nodes[j].links.push(NodeLink { toNode: to_name_id.clone(), weigth: weight });
+            j_found = true;
+          }  
+          j += 1;
+        }
+        i_found = true;
+      }
+      i += 1;
+    }
+
+    return !i_found || !j_found;
+  }
+
+  pub fn dijkstra(&mut self, name_id: String, to_name_id: String) -> i32 {
+
+    // REMETTRE LES POIDS A MAX OU 0
+    for i in 0..self.nodes.len() {
+      if self.nodes[i].name_id == name_id {
+        self.nodes[i].calculWeight = 0;
+      } else {
+        self.nodes[i].calculWeight = i32::MAX;
+      }
+    }
+
+    for j in 0..self.nodes.len() {
+      for k in 0..self.nodes[j].links.len() {
+        
+        // ON RECUPERE L'INDEX DU NOEUD DE DESTINATION
+        let mut x = 0;
+        while x < self.nodes.len() && self.nodes[x].name_id != self.nodes[j].links[k].toNode {
+          x += 1;
+        }
+
+        // Si le " POIDS DU NOEUD TRAITE + LE POIDS DU CHEMIN " est inférieur au " NOEUD DE DESTINATION "
+        // On update le POIDS DU NOEUD SUIVANT avec la nouvelle valeur inférieur
+        if self.nodes[j].calculWeight + self.nodes[j].links[k].weigth < self.nodes[x].calculWeight {
+          self.nodes[x].calculWeight = self.nodes[j].calculWeight + self.nodes[j].links[k].weigth;
+        }
+      }
+    }
+
+    // ON RECUPERE L'INDEX DU NOEUD DE FIN
+    let mut x = 0;
+    while x < self.nodes.len() && self.nodes[x].name_id != to_name_id {
+      x += 1;
+    }
+
+    return self.nodes[x].calculWeight;
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod graph;
+use graph::Graph;
+
 fn main() {
-    println!("Hello, world!");
+  let mut graphTest = Graph::new();
+
+  graphTest.addPoint(String::from("A"));
+  graphTest.addPoint(String::from("B"));
+  graphTest.addPoint(String::from("C"));
+  graphTest.addPoint(String::from("D"));
+  graphTest.addPoint(String::from("E"));
+  graphTest.addPoint(String::from("F"));
+
+  graphTest.linkPoints(String::from("A"), String::from("B"), 4);
+  graphTest.linkPoints(String::from("A"), String::from("C"), 4);
+  graphTest.linkPoints(String::from("B"), String::from("C"), 2);
+  graphTest.linkPoints(String::from("C"), String::from("D"), 3);
+  graphTest.linkPoints(String::from("C"), String::from("E"), 1);
+  graphTest.linkPoints(String::from("D"), String::from("F"), 2);
+  graphTest.linkPoints(String::from("E"), String::from("F"), 3);
+
+  let test = graphTest.dijkstra(String::from("A"), String::from("F"));
+  graphTest.show();
+  println!("Poids du chemin optimisé: {:?}", test);
 }
